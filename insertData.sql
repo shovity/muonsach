97f5521ca5ed1b9ef9fdb53c8766c5f11fb28197
shovity <shovity@gmail.com>
2017-12-06 15:44:23 +0700

    add insertData.sql

---
+++ insertData.sql
@@ -0,0 +1,21 @@
+﻿use muonsach
+-- select * from sach
+-- https://tiki.vn/inuyasha-tap-2-ban-dac-biet-p1005470.html?src=category-page
+
+insert into tac_gia values('Rumiko Takahashi')
+insert into nxb values('Nha xuat ban tre')
+
+insert into sach(ten, anh, ma_tac_gia, ma_nxb, so_trang, gia, gia_thue, mo_ta, nam_xb, ngay_nhap)
+values(
+	N'Inuyasha - Tập 2 (Bản Đặc Biệt)',
+	N'3.u5465.d20171009.t095206.104725.jpg',
+	1,
+	1,
+	120,
+	48000,
+	1000,
+	N'no des',
+	'1999',
+	'6/12/2017'
+)
+
